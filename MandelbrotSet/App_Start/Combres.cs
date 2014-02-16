05a3e0c28db60645c4eaec6bd2a2ea88af213ef0
Marius Schulz <marius.schulz@me.com>
2014-02-16 17:37:53 +0100

    - updated jQuery
    - removed knockout
    - removed TypeScript definition files
    - removed unnecessary code files
    - fixed tabs vs. spaces

--- MandelbrotSet/App_Start/Combres.cs
+++ MandelbrotSet/App_Start/Combres.cs
@@ -1,10 +1,13 @@
 [assembly: WebActivator.PreApplicationStartMethod(typeof(MandelbrotSet.App_Start.Combres), "PreStart")]
-namespace MandelbrotSet.App_Start {
-	using System.Web.Routing;
-	using global::Combres;
-	
-    public static class Combres {
-        public static void PreStart() {
+namespace MandelbrotSet.App_Start
+{
+    using System.Web.Routing;
+    using global::Combres;
+
+    public static class Combres
+    {
+        public static void PreStart()
+        {
             RouteTable.Routes.AddCombresRoute("Combres");
         }
     }
